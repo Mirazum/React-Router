ad53c62c61017eab17cef17fef95d99373875e05
Mirazum Parvin <mira.sust@gmail.com>
2023-04-04 19:18:20 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './Components/Home'
+import About from './Components/About'
+import Books from './Components/Books'
+import BookDetails from './Components/BookDetails'
+import LoadingSpinner from './Components/LoadingSpinner'
+import ErrorPage from './Components/ErrorPage'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App></App>,
+    errorElement: <ErrorPage />,
+    children:[{
+      path: '/',     //default hisabe eta dekhabe//
+      element: <Home></Home>
+    },
+    {
+      path:'books',
+      element: <Books></Books>,
+      loader:()=> fetch('https://api.itbook.store/1.0/new'),
+
+    },
+    {
+      path: 'book/:id',
+      element: <BookDetails />,
+      loader: ({ params }) =>
+        fetch(`https://api.itbook.store/1.0/books/${params.id}`),
+    },
+    {
+      path: '/about',
+      element:<About></About>
+    }
+   
+  ]
+  },
+  {
+    path: 'loader',
+    element: <LoadingSpinner />,
+  },
+])
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
